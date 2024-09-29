5dfa7baec25438a4f1a4c8f55095137fd0d99031
Michu444 <m.m.dudziak.2@gmail.com>
2024-09-29 22:45:46 +0200

    Added env var to decide if containers should start or not

--- app/app.py
+++ app/app.py
@@ -38,14 +38,17 @@ if __name__ == "__main__":
     if APP_DEBUG_MODE_ENABLED:
         logger.info("APP DEBUG MODE IS ENABLED!")
     docker_compose_path = "docker-compose.yml"
+    containers_start_parallel = os.getenv('CONTAINERS_START_PARALLEL')
     dm = DockerManager()
 
     try:
-        dm.run_docker_services(docker_compose_path)
-        dm.wait_for_mongo_container_start()
+        if containers_start_parallel:
+            dm.run_docker_services(docker_compose_path)
+            dm.wait_for_mongo_container_start()
 
         create_and_run_app()
     except Exception as e:
         logger.error(f"An unexpected error occurred! Error: {e}")
     finally:
-        dm.stop_docker_services(docker_compose_path)
+        if containers_start_parallel:
+            dm.stop_docker_services(docker_compose_path)
