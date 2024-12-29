11c9e2550551bacb51805c93b40be4d22f32fecf
martabartsch <martabartsch@outlook.com>
2024-12-29 13:51:02 +0100

    Changed name of method

--- app/Mongo/mongo_manager.py
+++ app/Mongo/mongo_manager.py
@@ -17,11 +17,38 @@ class MongoManager:
         try:
             self.client = MongoClient(self.url)
             self.db = self.client['menu_database']
-            self.collection = self.db['menu']
-            self.order_collection = self.db['order']
+            self.menu_collection = self.db['menu']
             logger.info("Connected to MongoDB successfully!")
         except Exception as e:
             logger.error(f"Failed to connect to MongoDB: {e}")
+
+    def add_record(self, row_data: dict):
+        """
+            Add a new row of admin input data.
+        """
+        if "_id" in row_data:
+            del row_data["_id"]
+        return self.menu_collection.insert_one(row_data).inserted_id
+
+    def update_record(self, row_data: dict):
+        """
+            Update user-entered data.
+        """
+        object_id = ObjectId(row_data['_id'])
+        update_data = {key: value for key, value in row_data.items() if key != "_id"}
+        return self.menu_collection.update_one({'_id': object_id}, {'$set': update_data})
+
+    def delete_record(self, dish_id: str):
+        """
+            Delete record from database by dish_id.
+        """
+        try:
+            object_id = ObjectId(dish_id)
+            result = self.menu_collection.delete_one({"_id": object_id})
+            return result.deleted_count > 0
+        except Exception as e:
+            logger.error(f"Error with deleting record: {e}")
+            return False
 
     def check_mongo_connection(self) -> bool:
         """
@@ -37,38 +64,10 @@ class MongoManager:
 
 
     def get_order_list(self):
-        return list(self.order_collection.find({}))
-
-    def add_user_record(self, row_data):
-        """
-            Add a new row of admin input data.
-        """
-        if "_id" in row_data:
-            del row_data["_id"]
-        return self.order_collection.insert_one(row_data).inserted_id
+        return list(self.menu_collection.find({}))
 
     def get_dish_by_id(self, dish_id):
         """
             Get all dish data from the database by its ID.
         """
-        return self.order_collection.find_one({"_id": ObjectId(dish_id)})
-
-    def update_record(self, row_data: dict):
-        """
-            Update user-entered data.
-        """
-        object_id = ObjectId(row_data['_id'])
-        update_data = {key: value for key, value in row_data.items() if key != "_id"}
-        return self.order_collection.update_one({'_id': object_id}, {'$set': update_data})
-
-    def delete_record_from_db(self, dish_id):
-        """
-            Delete order from database.
-        """
-        try:
-            object_id = ObjectId(dish_id)
-            result = self.order_collection.delete_one({"_id": object_id})
-            return result.deleted_count > 0
-        except Exception as e:
-            logger.error(f"Error occurred while deleting record: {e}")
-            return False
+        return self.menu_collection.find_one({"_id": ObjectId(dish_id)})
